950a1ded0c6e5e5e51f219c2c8f7846f782171cd
aznprox <aznprox@hotmail.com>
2018-05-15 23:27:44 -0400

    linked server.js to reactdev

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  var payload = fs.readFileSync('./data_set.json', 'utf8');
-  res.end(payload);
-}).listen(1337);
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   var payload = fs.readFileSync('./data_set.json', 'utf8');
+//   res.end(payload);
+// }).listen(1337);
 
-console.log('Server started...');
+// console.log('Server started...');
+
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 1337;
+
+var payload = fs.readFileSync('./data_set.json', 'utf8');
+
+app.get('/api/yolo', (req, res) => {
+  res.send(payload);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
